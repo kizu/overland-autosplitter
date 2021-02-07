d821b10e19250632efcaa10d2d8afb5ffcaae540
Roman Komarov <kizmarh@ya.ru>
2021-02-07 20:41:00 +0100

    Basic styles

--- src/components/RunStatsTable.tsx
+++ src/components/RunStatsTable.tsx
@@ -5,58 +5,14 @@ import React from 'react'
 import type { RunStats, Segment } from '../lib/types';
 import { BIOME_NAME_MAP } from '../lib/constants';
 import { Timer } from './Timer';
+import { styles } from './RunStatsTable.styles';
 
-import styled, { css } from '@reshadow/react'
+import styled from '@reshadow/react'
 
 interface RunStatsTableProps {
   run?: RunStats;
   finalTimeStamp?: number;
   segments: Segment[];
-}
-
-interface Variable {
-  label: string;
-  value: string;
-}
-
-const getCategoryAndVariables = ({ settings, biomeName }: RunStats) => {
-  let category = 'unknown';
-  let variables: Variable[] = [];
-
-  if (biomeName !== 'city') {
-    variables.push({
-      label: 'IL',
-      value: BIOME_NAME_MAP[biomeName]
-    })
-  }
-
-  if (settings.allDogsMode) {
-    variables.push({ label: 'All Dogs', value: 'yes' });
-  }
-
-  if (settings.difficulty === 1) {
-    if (settings.touristMode) {
-      category = 'Tourist';
-    } else {
-      category = 'Any%';
-    }
-  }
-
-  if (settings.difficulty === 2) {
-    category = 'Hard';
-  }
-
-  if (settings.difficulty === 3) {
-    if (settings.alwaysNightMode && settings.extraCreaturesMode && settings.noReviveMode && settings.noStunMode && settings.turnTimerEnabled) {
-      category = 'Ultimate';
-    } else {
-      category = 'Expert';
-    }
-  }
-
-  // Other variables are not yet displayed, so we're not pushing them
-
-  return [{ label: 'Category', value: category }, ...variables]
 }
 
 const getRunMeta = ({ buildNumber, settings, biomeName }: RunStats, isForcedIL: boolean) => {
@@ -104,45 +60,44 @@ export const RunStatsTable = ({ run, finalTimeStamp, segments }: RunStatsTablePr
 
   const { canBeIL, buildNumber, isIL, category, isAllDogs } = getRunMeta(run, isForcedIL);
 
-  return styled`
-  h1 {
-    /* yay, works! */
-    background: pink;
-  }
-  `(<div>
+  return styled(styles)(<main>
     <h1>Overland <small>build {buildNumber}</small></h1>
     <h2>
-      {category}
+      <span>
+        {category}
+        {' '}
+        {canBeIL ? <label><input type="checkbox" checked={isForcedIL} onChange={() => setIsForcedIL(!isForcedIL)} /> IL?</label> : null}
+      </span>
       {' '}
-      {isAllDogs ? <small>(all dogs)</small> : null} {}
+      {isAllDogs ? <small>All Dogs!</small> : null} {}
       {' '}
-      {canBeIL ? <label><input type="checkbox" checked={isForcedIL} onChange={() => setIsForcedIL(!isForcedIL)} /> IL?</label> : null }
     </h2>
     <ol>
       {
         segments.map(({ name, start, end, subSegments }, index) => {
           const subItems = !(end && !isIL) && subSegments.length > 0 ? subSegments.map(({ name: subName, start: subStart, end: subEnd }, subIndex) => (
-            <li key={subIndex}>{subName}: <Timer from={subStart} finalTimeStamp={subEnd || finalTimeStamp} /></li>
+            <li key={subIndex}>
+              <span>{subName}</span>
+              <Timer from={subStart} finalTimeStamp={subEnd || finalTimeStamp} />
+            </li>
           )) : null;
 
-          return isIL ? index === 0 ? subItems : null : (<li key={index}>
-            {name}: <Timer from={start} finalTimeStamp={end || finalTimeStamp} />
-            {end ? `, stops: ${subSegments.length}` : null}
-
-            {subItems ? <ol>
-              {subItems}
-            </ol> : null}
-          </li>)
-        }
+          return isIL ? index === 0 ? subItems : null : (
+            <li key={index}>
+              <span>{name} {end ? <small>— {subSegments.length} stops</small> : null}</span>
+              <Timer from={start} finalTimeStamp={end || finalTimeStamp} />
+              {subItems ? <ol>{subItems}</ol> : null}
+            </li>
+            );
+          }
         )
       }
     </ol>
-    <p>
-      Big timer:
+    <output>
       { isIL
-        ? <Timer from={run.startDate} finalTimeStamp={segments[0]?.end || finalTimeStamp} />
-        : <Timer from={run.startDate} finalTimeStamp={finalTimeStamp} />
+        ? <Timer isLarge from={run.startDate} finalTimeStamp={segments[0]?.end || finalTimeStamp} />
+        : <Timer isLarge from={run.startDate} finalTimeStamp={finalTimeStamp} />
       }
-      </p>
-  </div>)
+    </output>
+  </main>)
 };
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,8 @@
+/* @jsx styled.jsx */
+
 import React from 'react'
 import { useInterval } from '../lib/useInterval'
+import styled, { css, use } from '@reshadow/react'
 
 const oneSecond = 1000;
 const oneMinute = oneSecond * 60;
@@ -8,9 +11,20 @@ const oneHour = oneMinute * 60;
 interface TimerProps {
   from: number;
   finalTimeStamp?: number;
+  isLarge?: boolean;
 }
 
-export const Timer = React.memo(({ from, finalTimeStamp }: TimerProps) => {
+const styles = css`
+  span[use|isLarge] {
+    font-size: var(--font-large);
+  }
+
+  small {
+    font-size: var(--font-regular);
+  }
+`;
+
+export const Timer = React.memo(({ from, finalTimeStamp, isLarge }: TimerProps) => {
   const [now, setNow] = React.useState(Date.now);
   useInterval(() => setNow(Date.now), finalTimeStamp ? null : 16);
 
@@ -21,11 +35,11 @@ export const Timer = React.memo(({ from, finalTimeStamp }: TimerProps) => {
   const seconds = Math.floor((diff - hours * oneHour - minutes * oneMinute) / oneSecond);
   const milliseconds = diff - hours * oneHour - minutes * oneMinute - seconds * oneSecond;
   const hh = `${hours}`.padStart(2, '0');
-  const mm = `${minutes}`.padStart(2, '0');
+  const mm = `${minutes}`.padStart(hh !== '00' ? 2 : 1, '0');
   const ss = `${seconds}`.padStart(2, '0');
   // If the time is final, do not round ms, otherwise it is better rounded,
   // so it would look better in the animation.
   const ms = `${milliseconds}`.padStart(3, '0');
 
-  return <>{hh}:{mm}:{ss}.{ms}</>;
+  return styled(styles)(<span {...use({ isLarge })}>{hh !== '00' ? `${hh}:` : null}{mm}:{ss}{isLarge ? <small>{`.${ms}`}</small> : `.${ms}`}</span>);
 });
